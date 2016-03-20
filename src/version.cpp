ac784c3955e84c8415d2723b333bb4827ae758fb
scificrypto <realgrandnagus@gmail.com>
2016-03-20 11:58:25 -0500

    Updated checkpoints an version info

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("Latinum");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-Arridor"
+#define CLIENT_VERSION_SUFFIX   "-Quark"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
@@ -37,7 +37,7 @@ const std::string CLIENT_NAME("Latinum");
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
 #    define GIT_COMMIT_ID ""
-#    define GIT_COMMIT_DATE "18 October 2015"
+#    define GIT_COMMIT_DATE "20 March 2016"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
